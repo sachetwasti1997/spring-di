ecfcc24385f9747fc4fd33878699500e02988e12
sachetwasti1997 <sachetwasti61@gmail.com>
2021-05-25 10:56:29 +0530

    [ADD] Profiles added and the default profile set.

--- src/main/java/com/sachet/sfgdi/SfgDiApplication.java
+++ src/main/java/com/sachet/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package com.sachet.sfgdi;
 
-import com.sachet.sfgdi.controller.ConstructorInjectedController;
-import com.sachet.sfgdi.controller.MyController;
-import com.sachet.sfgdi.controller.PropertyInjectedController;
-import com.sachet.sfgdi.controller.SetterInjectedController;
+import com.sachet.sfgdi.controller.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,6 +11,9 @@ public class SfgDiApplication {
 	public static void main(String[] args) {
 
 		ApplicationContext applicationContext = SpringApplication.run(SfgDiApplication.class, args);
+
+		I18nController i18nController = (I18nController) applicationContext.getBean("i18nController");
+		System.out.println(i18nController.getGreeting());
 
 		MyController controller = (MyController) applicationContext.getBean("myController");
 
---
+++ src/main/java/com/sachet/sfgdi/controller/I18nController.java
@@ -0,0 +1,19 @@
+package com.sachet.sfgdi.controller;
+
+import com.sachet.sfgdi.service.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class I18nController {
+
+    private final GreetingService greetingService;
+
+    public I18nController(@Qualifier("i18nService") GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting(){
+        return greetingService.sayGreetings();
+    }
+}
